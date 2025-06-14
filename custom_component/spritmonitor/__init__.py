29b8075dfa1a7ff2ac29bb810ed31d3fdd7b0389
matbott <matiasfusaro@gmail.com>
2025-06-14 16:23:22 -0300

    Update __init__.py

--- custom_component/spritmonitor/__init__.py
+++ custom_component/spritmonitor/__init__.py
@@ -87,4 +87,4 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
 
-    return unload_ok#         
+    return unload_ok       
